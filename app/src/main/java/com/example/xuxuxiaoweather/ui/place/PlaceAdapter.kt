42f08943c3298f6450f20b0ef5767c3ce9405343
crazyfootman <txwyzxx@163.com>
2022-08-26 11:52:08 +0800

    新增侧拉栏切换天气

--- app/src/main/java/com/example/xuxuxiaoweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/example/xuxuxiaoweather/ui/place/PlaceAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.xuxuxiaoweather.R
 import com.example.xuxuxiaoweather.logic.model.Place
 import com.example.xuxuxiaoweather.ui.weather.WeatherActivity
+import kotlinx.android.synthetic.main.activity_weather.*
 
 class PlaceAdapter(private val fragment: PlaceFragment,private val placeList:List<Place>):
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
@@ -29,15 +30,26 @@ class PlaceAdapter(private val fragment: PlaceFragment,private val placeList:Lis
         holder.itemView.setOnClickListener{
             val position = holder.adapterPosition
             val place = placeslist[position]
-            val intent = Intent(parent.context,WeatherActivity::class.java).apply {
-                putExtra("location_lng",place.location.lng)
-                putExtra("location_lat",place.location.lat)
-                putExtra("place_name",place.name)
+            val activity = fragment.activity
+            if(activity is WeatherActivity){
+                activity.drawerlayout.closeDrawers()
+                activity.viewModel.locationlng = place.location.lng
+                activity.viewModel.locationlat = place.location.lat
+                activity.viewModel.placeName = place.name
+                activity.refreshWeather()
+            }else{
+                val intent = Intent(parent.context,WeatherActivity::class.java).apply {
+                    putExtra("location_lng",place.location.lng)
+                    putExtra("location_lat",place.location.lat)
+                    putExtra("place_name",place.name)
+                }
+                fragment.viewModel.savePlace(place)
+                fragment.startActivity(intent)
+                activity?.finish()
             }
 
-            fragment.viewModel.savePlace(place)
-            fragment.startActivity(intent)
-            fragment.activity?.finish()
+
+
         }
         return holder
     }
--- app/src/main/java/com/example/xuxuxiaoweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/xuxuxiaoweather/ui/weather/WeatherActivity.kt
@@ -1,13 +1,17 @@
 package com.example.xuxuxiaoweather.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.xuxuxiaoweather.R
@@ -21,7 +25,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class WeatherActivity : AppCompatActivity() {
-    private val viewModel by lazy { ViewModelProvider(this).get(WeatherViewModel::class.java) }
+     val viewModel by lazy { ViewModelProvider(this).get(WeatherViewModel::class.java) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val decorView = window.decorView
@@ -46,8 +50,38 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            swipRefresh.isRefreshing = false
         })
+        swipRefresh.setColorSchemeResources(R.color.colorPrimary)
+        refreshWeather()
+        swipRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+        navbtn.setOnClickListener {
+            drawerlayout.openDrawer(GravityCompat.START)
+        }
+        drawerlayout.addDrawerListener(object:DrawerLayout.DrawerListener{
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                val manager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken,InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+            }
+        })
+    }
+
+    fun refreshWeather(){
         viewModel.refreshWeather(viewModel.locationlng,viewModel.locationlat)
+        swipRefresh.isRefreshing = true
     }
     private fun showWeatherInfo(weather: Weather){
         placeName.text = viewModel.placeName
